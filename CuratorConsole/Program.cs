b3ebb7c522fa6f590e07bb645bc361f464bf2cb1
David L Valentine <davidlewisvalentine@gmail.com>
2023-02-12 16:03:48 -0800

    First attempt at making the string better

--- CuratorConsole/Program.cs
+++ CuratorConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
@@ -21,7 +20,7 @@ namespace CuratorConsole
             // TODO -> token should be permanent, but easy to regen if needed. Might want to document we used Reddit.NET's built in retriever to get it
             var reddit = new RedditClient(appId: "appId", appSecret: "appSecret", refreshToken: "refreshToken");
 
-            // Randomly select sub and get random "hot" post from the first 10
+            // Randomly select sub and get random "hot" post from the first 15
             Random rnd = new Random();
             int subChoice = rnd.Next(0, 2);
 
@@ -58,14 +57,42 @@ namespace CuratorConsole
             System.Drawing.Image image = System.Drawing.Image.FromStream(fs);
             fs.Close();
 
-            System.Drawing.Bitmap b = new System.Drawing.Bitmap(image);
+
+            // NOTE: Not perfect, but first attempt at image resizing to make the title easier to read
+            // TODO: Figure out a better way to resize images at odd resolutions
+            // NOTE: If this bothers me, then I can just work with the font size rather than the image size
+            int desiredWidth = image.Width;
+            int desiredHeight = image.Height;
+
+            if(image.Width < 2160)
+            {
+                // If image isn't 4k, resize to 1080
+                desiredWidth = 1920;
+                desiredHeight = 1080;
+            } 
+            else if (image.Width < 1920)
+            {
+                // if Image isn't 1080, resize to 720
+                desiredWidth = 1280;
+                desiredHeight = 720;
+            }
+
+            // Take the downloaded image, resize it, and then draw the title/post title on the top left corner
+
+            System.Drawing.Bitmap b = Utility.ResizeImage(image, desiredWidth, desiredHeight);
             System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(b);
             System.Drawing.FontFamily family = new System.Drawing.FontFamily("Times New Roman");
 
-            // TODO -> Need to set font size based on size of image, ideally a percentage.
             System.Drawing.Font font = new System.Drawing.Font(family, 30.0f, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline);
             
-            // TODO -> Would be nice if I could determine the predominant color in the image and set the color from that to contrast better
+
+            System.Drawing.SolidBrush semiTransparentBrush = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(125, 0, 0, 0));
+
+            var stringSizeByFont = graphics.MeasureString(postTitle, font);
+
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, (int)stringSizeByFont.Width, (int)stringSizeByFont.Height);
+            graphics.FillRectangle(semiTransparentBrush, rect);
+
             graphics.DrawString($"{postTitle}", font, System.Drawing.Brushes.White, 0, 0);
 
             b.Save($"{appdata}\\curatorPick.jpg", image.RawFormat);
