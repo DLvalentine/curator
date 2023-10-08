99e074f36f2958742a0148d78145f27e40d2b95f
David L Valentine <davidlewisvalentine@gmail.com>
2023-10-07 21:00:05 -0700

    Attempt at fixing shitty image resizing

--- CuratorConsole/Program.cs
+++ CuratorConsole/Program.cs
@@ -4,8 +4,8 @@ using System.Net;
 using System.Runtime.InteropServices;
 using Reddit;
 
-// NOTE -> Currently just proof of concept. Ideally this would be a bit more OO with methods, etc. Procedural is fine for now.
-//         Just getting my feet wet with the Reddit API for use later.
+// Originally meant to be my first foray into VR, to make a "virtual museum"
+// Now just a wallpaper updater lmao. Maybe one day.
 namespace CuratorConsole
 {
     class Program
@@ -17,15 +17,15 @@ namespace CuratorConsole
 
         static void Main(string[] args)
         {
-            // TODO -> token should be permanent, but easy to regen if needed. Might want to document we used Reddit.NET's built in retriever to get it
-            var reddit = new RedditClient(appId: "appId", appSecret: "appSecret", refreshToken: "refreshToken");
+            var reddit = new RedditClient(appId: "", appSecret: "", refreshToken: "");
 
             // Randomly select sub and get random "hot" post from the first 15
             Random rnd = new Random();
-            int subChoice = rnd.Next(0, 2);
+            int subChoice = rnd.Next(0, 6);
 
             var sub = reddit.Subreddit();
 
+            // TODO : Find more subs to source from!
             switch(subChoice)
             {
                 case 0:
@@ -37,6 +37,18 @@ namespace CuratorConsole
                 case 2:
                     sub = reddit.Subreddit("ClassicArt");
                     break;
+                case 3:
+                    sub = reddit.Subreddit("BirdPhotography");
+                    break;
+                case 4:
+                    sub = reddit.Subreddit("Painting");
+                    break;
+                case 5:
+                    sub = reddit.Subreddit("Drawing");
+                    break;
+                case 6:
+                    sub = reddit.Subreddit("ImaginaryLandscapes");
+                    break;
             }
 
             int postChoice = rnd.Next(0, 15);
@@ -47,10 +59,12 @@ namespace CuratorConsole
             string postTitle = post.Listing.Title;
 
             // Save to appdata
-            // NOTE/TODO -> Probably requires admin privileges...might need to document this or add to manifest so users get prompted
             var appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             var webClient = new WebClient();
-            webClient.DownloadFile(postURL, $"{appdata}\\curatorPick.jpg");
+            try
+            {
+                webClient.DownloadFile(postURL, $"{appdata}\\curatorPick.jpg");
+            } catch (Exception e) { Console.WriteLine("Unable to download image, might be a text post."); }
             webClient.Dispose();
 
             FileStream fs = new FileStream($"{appdata}\\curatorPick.jpg", FileMode.Open, FileAccess.Read);
@@ -58,32 +72,38 @@ namespace CuratorConsole
             fs.Close();
 
 
-            // NOTE: Not perfect, but first attempt at image resizing to make the title easier to read
-            // TODO: Figure out a better way to resize images at odd resolutions
-            // NOTE: If this bothers me, then I can just work with the font size rather than the image size
+            // NOTE: Resizing isn't perfect, but mainly because some images from reddit have funky dimensions
             int desiredWidth = image.Width;
             int desiredHeight = image.Height;
+            float fontSize = 30.0f;
 
-            if(image.Width < 2160)
-            {
-                // If image isn't 4k, resize to 1080
-                desiredWidth = 1920;
-                desiredHeight = 1080;
-            } 
-            else if (image.Width < 1920)
+            if (image.Width < 1920)
             {
                 // if Image isn't 1080, resize to 720
                 desiredWidth = 1280;
                 desiredHeight = 720;
+                fontSize = 25.0f;
             }
+            else if(image.Width < 2160)
+            {
+                // If image isn't 4k, resize to 1080
+                desiredWidth = 1920;
+                desiredHeight = 1080;
+                fontSize = 30.0f;
+            }
+            else if(image.Width >= 2160)
+            {
+                // Big image. Don't resize, but bump up the font size a touch
+                fontSize = 45.0f;
+            }
+           
 
             // Take the downloaded image, resize it, and then draw the title/post title on the top left corner
-
             System.Drawing.Bitmap b = Utility.ResizeImage(image, desiredWidth, desiredHeight);
             System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(b);
             System.Drawing.FontFamily family = new System.Drawing.FontFamily("Times New Roman");
 
-            System.Drawing.Font font = new System.Drawing.Font(family, 30.0f, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline);
+            System.Drawing.Font font = new System.Drawing.Font(family, fontSize, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline);
             
 
             System.Drawing.SolidBrush semiTransparentBrush = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(125, 0, 0, 0));
